ed71fc220330c1bbf4ff86f0f105567b559320b1
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-01-21 17:50:29 -0400

    Implement custom hook

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -11,13 +11,14 @@ export default function InterviewerList(props) {
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">{props.interviewers.map(interviewer => 
 
-        <InterviewerListItem
-          key={interviewer.id}
-          name={interviewer.name}
-          avatar={interviewer.avatar}
-          selected={interviewer.id === props.interviewer}
-          setInterviewer={interviewer.id}
-        />
+      <InterviewerListItem
+        key            = {interviewer.id}
+        name           = {interviewer.name}
+        avatar         = {interviewer.avatar}
+        selected       = {interviewer.id === props.value}
+        setInterviewer = {() => props.onChange(interviewer.id)}
+      />
+
       )}
 
       </ul>
